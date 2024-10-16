c8bc76d7136ca52d3c4333e1787159f629758cc4
thjbbvlt <thibault2@ik.me>
2024-10-16 12:07:17 +0200

    fix lua filter

--- luafilter/pandoc-comma-comment.lua
+++ luafilter/pandoc-comma-comment.lua
@@ -1,43 +1,29 @@
-function Inlines(inlines)
-    local count_opening = 0
-    local count_closing = 0
-    local opening = true
-    local delete
-    if inlines == nil then return inlines end
-    for n=1, #inlines do
-        local r = ""
-        local s = inlines[n].text
-
-        if s ~= nil then
-            if string.match(s, ',,') then
-                for i=1, #s do
-                    local char = string.sub(s, i, i)
-                    if char == ","
-                        and string.sub(s, i+1, i+1) == ","
-                        and delete ~= i then
-                        delete = i + 1
-                        if opening == true then
-                            r = r .. "<!--"
-                            opening = false
-                            count_opening = count_opening + 1
-                        else
-                            r = r .. "-->"
-                            count_closing = count_closing + 1
-                            opening = true
-                        end
-                    elseif delete == i then
-                        delete = nil
-                    else
-                        r = r .. char
-                    end
-                    inlines[n] = pandoc.RawInline('html', r)
-                end
-            end
+local function remove_comma_comment(s, iscomment)
+    local new = ""
+    local prev = ''
+    local cur = ''
+    for i=1, #s do
+        prev = cur
+        cur = string.sub(s, i, i)
+        if prev == ',' and cur == ',' then
+            iscomment = not iscomment
+        elseif cur ~= ',' and iscomment ~= true then
+            new = new .. cur
         end
     end
-    if count_closing == count_opening then
-        return inlines
-    else
-        return nil
+    return new, iscomment
+end
+
+function Inlines(inlines)
+    local iscomment = false
+    local new = nil
+    if inlines == nil then return inlines end
+    for n=1, #inlines do
+        local s = inlines[n].text
+        if s ~= nil then
+            new, iscomment = remove_comma_comment(s, iscomment)
+            inlines[n].text = new
+        end
     end
+    return inlines
 end
